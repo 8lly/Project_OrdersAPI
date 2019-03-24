623978e8fabb57fd643103671c1b34ff1d98655f
olly <oliver_atkins@outlook.com>
2019-03-24 12:01:28 +0000

    Created - GetOrders Tests

--- OrdersAPITests/OrdersProviderTests/GetOrdersTests.cs
+++ OrdersAPITests/OrdersProviderTests/GetOrdersTests.cs
@@ -1,4 +1,5 @@
 using Moq;
+using Newtonsoft.Json;
 using NUnit.Framework;
 using OrdersAPI.Models;
 using StockAPI.Models;
@@ -20,16 +21,49 @@ namespace OrdersAPITests.OrdersProviderTests
 
         // Test: Get one valid order
         [Test]
-        public void TestGetOrdersReturnsOneItem()
+        public void TestGetOrdersReturnsOneOrder()
         {
             // Act
             _mockOrderRepository.Setup(x => x.GetOrders()).Returns(CreateOrderDTOList(1));
-            // Why is this broke?
-            OrdersController orderProvider = new OrdersController(_mockOrderRepository.Object);
+            // Is this meant to be orderprovider or orderscontroller
+            OrdersProvider orderProvider = new OrdersProvider(_mockOrderRepository.Object);
+            ProviderResponseWrapperCopy outputGetOrdersAsProviderResponseWrapper = orderProvider.GetOrders();
+            List<OrderDTO> outputOrderList = JsonConvert.DeserializeObject<List<OrderDTO>>(outputGetOrdersAsProviderResponseWrapper.ResponseMessage);
 
+            // Assert
+            Assert.AreEqual(1, outputOrderList.Count);
+            Assert.AreEqual("Test0", outputOrderList[0].Id);
+        }
+
+        // Test: Get one thousand valid orders
+        [Test]
+        public void TestGetOrdersReturnsOneThousandOrders()
+        {
+            // Act
+            _mockOrderRepository.Setup(x => x.GetOrders()).Returns(CreateOrderDTOList(1000));
+            OrdersProvider orderProvider = new OrdersProvider(_mockOrderRepository.Object);
+            ProviderResponseWrapperCopy outputGetOrdersAsProviderResponseWrapper = orderProvider.GetOrders();
+            List<OrderDTO> outputOrderList = JsonConvert.DeserializeObject<List<OrderDTO>>(outputGetOrdersAsProviderResponseWrapper.ResponseMessage);
+
+            // Assert
+            Assert.AreEqual(1000, outputOrderList.Count);
+            for (int i = 0; i < outputOrderList.Count; i++)
+            {
+                Assert.AreEqual("Test" + i.ToString(), outputOrderList[i].Id);
+            }
+        }
+
+        // Test: Get null as response from 
+        [Test]
+        public void TestGetOrdersReturnsNullOrders()
+        {
+            // Act
+            _mockOrderRepository.Setup(x => x.GetOrders()).Returns(CreateOrderDTOList(0));
+            OrdersProvider orderProvider = new OrdersProvider(_mockOrderRepository.Object);
             ProviderResponseWrapperCopy outputGetOrdersAsProviderResponseWrapper = orderProvider.GetOrders();
 
-
+            // Assert
+            Assert.AreEqual("No orders have been saved!", outputGetOrdersAsProviderResponseWrapper.ResponseMessage);
         }
 
         private List<OrderDTO> CreateOrderDTOList(int numOfOrder)
